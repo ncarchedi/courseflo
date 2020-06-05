6cd8a13c168782dc918f2c504804172af0fecbf1
Nick Carchedi <nick.carchedi@gmail.com>
2020-06-05 17:01:49 -0600

    make video size reactive

--- src/items/Video.js
+++ src/items/Video.js
@@ -1,8 +1,22 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles((theme) => ({
+  box: {
+    overflow: "hidden",
+    paddingTop: "56.25%",
+    position: "relative",
+    "& iframe": {
+      position: "absolute",
+      height: "100%",
+      width: "100%",
+      left: 0,
+      top: 0,
+      border: 0,
+    },
+  },
   title: {
     marginBottom: theme.spacing(2),
   },
@@ -16,15 +30,15 @@ export default function Video({ item }) {
       <Typography className={classes.title} variant="h6">
         {item.title}
       </Typography>
-      <iframe
-        title={item.title}
-        src={item.source}
-        width="560"
-        height="315"
-        frameBorder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
+      <Box className={classes.box}>
+        <iframe
+          title={item.title}
+          src={item.source}
+          frameBorder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </Box>
     </>
   );
 }
