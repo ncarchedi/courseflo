2ea52fb2fd06f3f0c57c7b65e425835be5266f80
Nick Carchedi <nick.carchedi@gmail.com>
2020-06-28 10:04:06 -0600

    add snackbar for publishing changes

--- src/components/PublishButton.js
+++ src/components/PublishButton.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
@@ -9,78 +9,99 @@ import Paper from "@material-ui/core/Paper";
 import Popper from "@material-ui/core/Popper";
 import MenuItem from "@material-ui/core/MenuItem";
 import MenuList from "@material-ui/core/MenuList";
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab/Alert";
 
 export default function PublishButton({ onPublish, onRestore }) {
-  const [open, setOpen] = React.useState(false);
-  const anchorRef = React.useRef(null);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const anchorRef = useRef(null);
 
   const handleToggle = () => {
-    setOpen((prevOpen) => !prevOpen);
+    setMenuOpen((prevOpen) => !prevOpen);
   };
 
   const handleClose = (event) => {
     if (anchorRef.current && anchorRef.current.contains(event.target)) return;
-    setOpen(false);
+    setMenuOpen(false);
   };
 
   const handleClickPublish = () => {
-    setOpen(false);
+    setMenuOpen(false);
+    setSnackbarOpen(true);
     onPublish();
   };
 
   const handleClickRestore = () => {
-    setOpen(false);
+    setMenuOpen(false);
     onRestore();
   };
 
   return (
-    <Grid container direction="column" alignItems="center">
-      <Grid item xs={12}>
-        <ButtonGroup
-          variant="contained"
-          color="primary"
-          ref={anchorRef}
-          aria-label="split button"
-        >
-          <Button onClick={handleClickPublish}>Publish</Button>
-          <Button
+    <>
+      <Grid container direction="column" alignItems="center">
+        <Grid item xs={12}>
+          <ButtonGroup
+            variant="contained"
             color="primary"
-            size="small"
-            aria-controls={open ? "split-button-menu" : undefined}
-            aria-expanded={open ? "true" : undefined}
-            aria-label="select merge strategy"
-            aria-haspopup="menu"
-            onClick={handleToggle}
+            ref={anchorRef}
+            aria-label="split button"
           >
-            <ArrowDropDownIcon />
-          </Button>
-        </ButtonGroup>
-        <Popper
-          open={open}
-          placement="bottom-end"
-          anchorEl={anchorRef.current}
-          role={undefined}
-          transition
-          disablePortal
-        >
-          {({ TransitionProps }) => (
-            <Grow
-              {...TransitionProps}
-              style={{ transformOrigin: "center top" }}
+            <Button onClick={handleClickPublish}>Publish</Button>
+            <Button
+              color="primary"
+              size="small"
+              aria-controls={menuOpen ? "split-button-menu" : undefined}
+              aria-expanded={menuOpen ? "true" : undefined}
+              aria-label="select merge strategy"
+              aria-haspopup="menu"
+              onClick={handleToggle}
             >
-              <Paper>
-                <ClickAwayListener onClickAway={handleClose}>
-                  <MenuList id="split-button-menu">
-                    <MenuItem onClick={handleClickRestore}>
-                      Restore to last published version
-                    </MenuItem>
-                  </MenuList>
-                </ClickAwayListener>
-              </Paper>
-            </Grow>
-          )}
-        </Popper>
+              <ArrowDropDownIcon />
+            </Button>
+          </ButtonGroup>
+          <Popper
+            open={menuOpen}
+            placement="bottom-end"
+            anchorEl={anchorRef.current}
+            role={undefined}
+            transition
+            disablePortal
+          >
+            {({ TransitionProps }) => (
+              <Grow
+                {...TransitionProps}
+                style={{ transformOrigin: "center top" }}
+              >
+                <Paper>
+                  <ClickAwayListener onClickAway={handleClose}>
+                    <MenuList id="split-button-menu">
+                      <MenuItem onClick={handleClickRestore}>
+                        Restore to last published version
+                      </MenuItem>
+                    </MenuList>
+                  </ClickAwayListener>
+                </Paper>
+              </Grow>
+            )}
+          </Popper>
+        </Grid>
       </Grid>
-    </Grid>
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={6000}
+        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+        onClose={() => setSnackbarOpen(false)}
+      >
+        <MuiAlert
+          elevation={6}
+          variant="filled"
+          onClose={() => setSnackbarOpen(false)}
+          severity="success"
+        >
+          Your changes have been published!
+        </MuiAlert>
+      </Snackbar>
+    </>
   );
 }
