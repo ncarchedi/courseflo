baf28fee1a2501b7fd75a47885c227d81a1dcdc3
Nick Carchedi <nick.carchedi@gmail.com>
2020-06-28 18:16:54 -0600

    make it possible to add options

--- src/editableItems/EditableMultiSelect.js
+++ src/editableItems/EditableMultiSelect.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Box from "@material-ui/core/Box";
 import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
 import IconButton from "@material-ui/core/IconButton";
 import CheckBoxOutlineBlankIcon from "@material-ui/icons/CheckBoxOutlineBlank";
 import ClearIcon from "@material-ui/icons/Clear";
@@ -20,6 +21,12 @@ export default function EditableMultiSelect({ item, onFocus, onChangeItem }) {
   const handleChangeOption = (index, value) => {
     const options = [...values.options];
     options[index] = value;
+    setValues({ ...values, options });
+  };
+
+  const handleAddOption = () => {
+    const options = [...values.options];
+    options.push("");
     setValues({ ...values, options });
   };
 
@@ -59,6 +66,14 @@ export default function EditableMultiSelect({ item, onFocus, onChangeItem }) {
           </Box>
         </Box>
       ))}
+      <Box display="flex" alignItems="center" marginTop={1.5}>
+        <Box marginRight={1} display="flex">
+          <CheckBoxOutlineBlankIcon color="disabled" />
+        </Box>
+        <Link variant="body1" color="textSecondary" onClick={handleAddOption}>
+          Add option
+        </Link>
+      </Box>
       <TextField
         name="solution"
         label="Solution"
