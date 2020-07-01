44b006113b8936978856293907625662d068e1e3
Nick Carchedi <nick.carchedi@gmail.com>
2020-07-01 07:48:53 -0600

    get styling for courses and new course in good shape

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,6 +2,7 @@ import React, { useState, useContext } from "react";
 import { Redirect, useParams } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
@@ -11,6 +12,7 @@ import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import ShareOutlinedIcon from "@material-ui/icons/ShareOutlined";
 import DeleteOutlinedIcon from "@material-ui/icons/DeleteOutlined";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
 import DashboardHeader from "./DashboardHeader";
 import NotFound from "./NotFound";
 import FeedbackModal from "./FeedbackModal";
@@ -19,23 +21,28 @@ import UserContext from "../context/UserContext";
 const useStyles = makeStyles((theme) => ({
   container: {
     margin: theme.spacing(2, "auto"),
+  },
+  cardContainer: {
     display: "flex",
-    justifyContent: "space-evenly",
     flexWrap: "wrap",
   },
   card: {
     width: 250,
-    margin: theme.spacing(2, "auto"),
-  },
-  cardContent: {
+    minHeight: 190,
+    margin: theme.spacing(2),
     textAlign: "center",
+    display: "flex",
+    flexDirection: "column",
+  },
+  cardActionArea: {
+    flexGrow: 1,
   },
   cardActions: {
     display: "flex",
     justifyContent: "space-evenly",
   },
-  shareIcon: {
-    flexGrow: 1,
+  createCardActionArea: {
+    height: "100%",
   },
 }));
 
@@ -57,7 +64,7 @@ export default function Dashboard() {
   const CourseCard = ({ title, updated }) => {
     return (
       <Card className={classes.card}>
-        <CardActionArea>
+        <CardActionArea className={classes.cardActionArea}>
           <CardContent className={classes.cardContent}>
             <Typography gutterBottom variant="h5" component="h2">
               {title}
@@ -83,26 +90,46 @@ export default function Dashboard() {
     );
   };
 
+  const CreateCard = () => {
+    return (
+      <Card className={classes.card}>
+        <CardActionArea className={classes.createCardActionArea}>
+          <Typography variant="h5" component="h2">
+            New Course
+          </Typography>
+          <Box marginTop={1}>
+            <AddCircleOutlineIcon fontSize="large" color="primary" />
+          </Box>
+        </CardActionArea>
+      </Card>
+    );
+  };
+
   const courses = [
     { id: 1, title: "My Really Cool Course", updated: "June 28, 2020" },
     { id: 2, title: "My Slightly Less Cool Course", updated: "June 25, 2020" },
-    { id: 3, title: "My Blah Course", updated: "September 19, 2019" },
-    { id: 4, title: "My Really Cool Course", updated: "June 28, 2020" },
-    { id: 5, title: "My Slightly Less Cool Course", updated: "June 25, 2020" },
-    { id: 6, title: "My Blah Course", updated: "September 19, 2019" },
+    {
+      id: 3,
+      title: "My Blah Course That Has a Crazy Long Title",
+      updated: "September 19, 2019",
+    },
+    { id: 4, title: "My Other Really Cool Course", updated: "May 12, 2019" },
   ];
 
   return (
     <>
       <DashboardHeader setShowFeedbackModal={setShowFeedbackModal} />
       <Container className={classes.container} maxWidth="md">
-        {courses.map((course) => (
-          <CourseCard
-            key={course.id} // todo: how to use actual course IDs?
-            title={course.title}
-            updated={course.updated}
-          />
-        ))}
+        <Box className={classes.cardContainer}>
+          {courses.map((course) => (
+            <CourseCard
+              key={course.id} // todo: how to use actual course IDs?
+              title={course.title}
+              updated={course.updated}
+            />
+          ))}
+          <CreateCard />
+        </Box>
       </Container>
       <FeedbackModal
         open={showFeedbackModal}
