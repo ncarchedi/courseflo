9cfb6d86cf3bfdf129d0b59c0645517a5120e7a9
Nick Carchedi <nick.carchedi@gmail.com>
2020-07-16 17:53:24 -0600

    add logo below item

--- src/components/ItemViewer.js
+++ src/components/ItemViewer.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import { Link as RouterLink } from "react-router-dom";
 import { makeStyles } from "@material-ui/styles";
 import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
 import Item from "./Item";
 import NoItems from "./NoItems";
 import CourseNav from "./CourseNav";
@@ -10,6 +12,17 @@ import CompleteCourseDialog from "./CompleteCourseDialog";
 const useStyles = makeStyles((theme) => ({
   item: {
     marginTop: theme.spacing(3),
+  },
+  poweredByBox: {
+    marginTop: theme.spacing(3),
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    textDecoration: "none",
+    color: "inherit",
+  },
+  logo: {
+    maxHeight: theme.spacing(2),
   },
 }));
 
@@ -49,6 +62,21 @@ export default function ItemViewer({
           userItem={userItem}
           onItemLoad={onItemLoad}
           onChangeAnswer={onChangeAnswer}
+        />
+      </Box>
+      <Box
+        className={classes.poweredByBox}
+        component={RouterLink}
+        to="/"
+        target="_blank"
+      >
+        <Box mr={0.5}>
+          <Typography variant="body2">Powered by</Typography>
+        </Box>
+        <img
+          className={classes.logo}
+          src="../logo-bw.png"
+          alt="Courseflo logo"
         />
       </Box>
       <CourseNav
