6a0f0d83333185d9f68f7088d5a44bcbf50f66c4
Nick Carchedi <nick.carchedi@gmail.com>
2020-07-16 18:01:53 -0600

    extract PoweredBy component

--- src/components/ItemViewer.js
+++ src/components/ItemViewer.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
-import { Link as RouterLink } from "react-router-dom";
 import { makeStyles } from "@material-ui/styles";
 import Box from "@material-ui/core/Box";
-import Typography from "@material-ui/core/Typography";
 import Item from "./Item";
+import PoweredBy from "./PoweredBy";
 import NoItems from "./NoItems";
 import CourseNav from "./CourseNav";
 import JumpToItemDialog from "./JumpToItemDialog";
@@ -12,17 +11,6 @@ import CompleteCourseDialog from "./CompleteCourseDialog";
 const useStyles = makeStyles((theme) => ({
   item: {
     marginTop: theme.spacing(3),
-  },
-  poweredByBox: {
-    marginTop: theme.spacing(3),
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    textDecoration: "none",
-    color: "inherit",
-  },
-  logo: {
-    maxHeight: theme.spacing(2),
   },
 }));
 
@@ -64,21 +52,7 @@ export default function ItemViewer({
           onChangeAnswer={onChangeAnswer}
         />
       </Box>
-      <Box
-        className={classes.poweredByBox}
-        component={RouterLink}
-        to="/"
-        target="_blank"
-      >
-        <Box mr={0.5}>
-          <Typography variant="body2">Powered by</Typography>
-        </Box>
-        <img
-          className={classes.logo}
-          src="../logo-bw.png"
-          alt="Courseflo logo"
-        />
-      </Box>
+      <PoweredBy />
       <CourseNav
         itemNumber={itemNumber}
         onChangeItemNumber={onChangeItemNumber}
